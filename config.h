d7a242396b145630e1ef029c7efcc701935a2400
senpai-10 <bmjfdrh@gmail.com>
2021-03-24 15:22:30 +0300

    config.h

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
-#define TempPath "~/wm/slstatus/temp.sh"
+#define TempScript "~/wm/slstatus/temp.sh"
 
 /* interval between updates (in ms) */
 const unsigned int interval = 1000;
@@ -69,8 +69,8 @@ static const struct arg args[] = {
 	/* function     format               	argument */
 	{ uptime      	,   " uptime %s | "         , NULL },
 	{ keymap        ,   "layout %s | "          , NULL },
-	{ run_command   ,   "%s"                    , TempPath },
-	{ cpu_perc 	,   "[CPU  %s%] | "        , NULL },
+	{ run_command   ,   "%s"                    , TempScript },
+	{ cpu_perc 	    ,   "[CPU  %s%] | "        , NULL },
 	{ ram_perc  	,   "[RAM  %s%] | "     	, NULL },
 	{ datetime  	,   "%s"                 	, "%Y/%m/%d %A %I:%M:%S %p " },
 };
