fe6d15af646f20495fe831dcbbc703b791f6c8bc
senpai-10 <bmjfdrh@gmail.com>
2021-03-27 01:38:09 +0300

    temp.sh

--- temp.sh
+++ temp.sh
@@ -1,7 +1,12 @@
 TempCat=$(cat /sys/class/thermal/thermal_zone0/temp)
 Temp=${TempCat:: -3}
 
-[[ "$Temp" -gt 80 ]] && echo -e "[CPU \uf769 $Temp°C] | "
-[[ "$Temp" -gt 70 ]] && echo -e "[CPU \uf2c8 $Temp°C] | "
-[[ "$Temp" -gt 40 ]] && echo -e "[CPU \uf2c9 $Temp°C] | "
-[[ "$Temp" -lt 40 ]] && echo -e "[CPU \uf76b $Temp°C] | "
+if [ "$Temp" -gt 80 ]; then
+    echo "[CPU  $Temp°C] | "
+elif [ "$Temp" -gt 70 ]; then
+    echo "[CPU  $Temp°C] | "
+elif [ "$Temp" -gt 40 ]; then
+    echo "[CPU  $Temp°C] | "
+else 
+    echo "[CPU  $Temp°C] | "
+fi
