f466036ca5b11545301e9fa85f834c830b87c18e
senpai-10 <bmjfdrh@gmail.com>
2021-04-06 13:34:33 +0300

    weather

--- weather.sh
+++ weather.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 weather_temp=$(curl wttr.in?format=%t)
+weather_temp=${weather_temp:: -2}
+weather_temp=${weather_temp: 1}
 
-echo -e "weather temperature $weather_temp | "
+[[ $weather_temp > 30 ]] && echo -e "weather ☼ $weather_temp | "
+[[ $weather_temp > 20 ]] && echo -e "weather ☀ $weather_temp | "
+[[ $weather_temp < 20 ]] && echo -e "weather ❄ $weather_temp | "
