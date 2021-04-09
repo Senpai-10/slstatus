b2c6bf4ae8acf49f1aced95f78c4b2757b33bfb1
senpai-10 <bmjfdrh@gmail.com>
2021-04-10 01:43:51 +0300

    move to scripts/

--- network.sh
+++
@@ -1,7 +0,0 @@
-rxcurrent="$(($(cat /sys/class/net/*/statistics/rx_bytes | paste -sd '+')))"
-txcurrent="$(($(cat /sys/class/net/*/statistics/tx_bytes | paste -sd '+')))"
-
-download=$(numfmt --to=iec-i --suffix=B $rxcurrent)
-upload=$(numfmt --to=iec-i --suffix=B $txcurrent)
-
-echo -e "[▼ $download / ▲ $upload] | "
